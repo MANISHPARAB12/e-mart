b8ea0f961d39bd3b1025884e283cd7be40935dda
Manish parab <promanish413@gmail.com>
2026-07-23 15:11:29 +0530

    code update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
-import Categories from "./Pages/Categories";
-import ProductCategory from "./Pages/ProductCategory";
+
+// import Categories from "./Pages/Categories";
+// import ProductCategory from "./Pages/ProductCategory";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/category/:category" element={<ProductCategory />} />
+      {/* <Route path="/categories" element={<Categories />} /> */}
+      {/* <Route path="/category/:category" element={<ProductCategory />} /> */}
     </Routes>
   );
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ProductCategoryTabs from "../Components/ProductCategoryTabs";
 import {Card} from "../Components/Card";
 import {Herosection} from "../Components/Herosection";
+import RandomProducts from "../Components/RandomProducts";
 
 function Home() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -14,6 +15,9 @@ function Home() {
       <ProductCategoryTabs
         setSelectedCategory={setSelectedCategory}
       />
+      
+      <RandomProducts/>
+
 
       <Card selectedCategory={selectedCategory} />
     </>
